654fcb6544a6127d4ff97ef6e9812f02c960b06f
Zain Ul Abdeen <zu4425@gmail.com>
2024-11-09 17:01:39 +0500

    new

--- WebApplication1/Controllers/catogreycontroller.cs
+++ WebApplication1/Controllers/catogreycontroller.cs
@@ -14,34 +14,26 @@ namespace WebApplication1.Controllers
         new Catogrey { Id = 2, Name = "HP", displayorder = 2 },
         new Catogrey { Id = 3, Name = "Dell", displayorder = 3 },
     };
-        // for read
+        
         [HttpGet]
         public IEnumerable<Catogrey> Get()
         {
             return listCategories;
         }
-        // for add
+        
         [HttpPost]
         public void post([FromBody]Catogrey cat)
         {
             listCategories.Add(cat);
         }
-        // for edit
-        // remember eidt by index number not id
-        // pass index number in url not id number in url 
-
+        
         [HttpPut ("{Id}")]
         public void put ( int Id,[FromBody] Catogrey cat)
         {
             listCategories[Id] = cat;
         }
-        // for Delete
-        // remember Delete by index number not id
-        // pass index number in url not id number in url 
-
         
-
-        [HttpDelete("{Id}")]
+      [HttpDelete("{Id}")]
         public void Delete(int Id)
         {
             listCategories.RemoveAt(Id);
