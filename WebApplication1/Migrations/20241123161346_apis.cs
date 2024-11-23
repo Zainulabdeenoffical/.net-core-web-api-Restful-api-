55c155675399217fecd5e2e1f10e9ddc557745f6
Zain Ul Abdeen <zu4425@gmail.com>
2024-11-23 21:45:50 +0500

    day 12

---
+++ WebApplication1/Migrations/20241123161346_apis.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class apis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookCOvers_BookWriters_bookWritersId",
+                table: "BookCOvers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "bookWritersId",
+                table: "BookCOvers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "bookWriterID",
+                table: "BookCOvers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookCOvers_BookWriters_bookWritersId",
+                table: "BookCOvers",
+                column: "bookWritersId",
+                principalTable: "BookWriters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookCOvers_BookWriters_bookWritersId",
+                table: "BookCOvers");
+
+            migrationBuilder.DropColumn(
+                name: "bookWriterID",
+                table: "BookCOvers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "bookWritersId",
+                table: "BookCOvers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookCOvers_BookWriters_bookWritersId",
+                table: "BookCOvers",
+                column: "bookWritersId",
+                principalTable: "BookWriters",
+                principalColumn: "Id");
+        }
+    }
+}
--- WebApplication1/Models/bookCover.cs
+++ WebApplication1/Models/bookCover.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
 
+        public int bookWriterID { get; set; }
+
+        public bookWriters bookWriters { get; set; }
         public ICollection<book> Books { get; set; }
 
     }
